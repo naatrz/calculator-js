c33e5d4da94b99885227db998d28b316ea78df59
Ana Beatriz Viana dos Santos <anabiavs@hotmail.com>
2025-07-04 17:02:37 -0300

    ✨ feat: implement JS event listeners

---
+++ src/script.js
@@ -0,0 +1,25 @@
+const previousOperationText = document.querySelector("#previous-operation");
+const currentOperationText = document.querySelector("#current-operation");
+const buttons = document.querySelectorAll("#buttons-container button");
+
+class Calculator {
+    constructor(previousOperationText, currentOperationText) {
+        this.previousOperationText = previousOperationText;
+        this.currentOperationText = currentOperationText;
+        this.currentOperation = "";
+    }
+}
+
+const calc = new Calculator(previousOperationText, currentOperationText);
+
+buttons.forEach ((btn) => {
+    btn.addEventListener("click", (e) => {
+        const value = e.target.innerText;
+
+        if(+value >=0 || value === ".") {
+            console.log(value);
+        } else {
+            console.log("Op: " + value);
+        }
+    });
+});
